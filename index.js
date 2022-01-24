ffa290e231cc28cf62fb07d16b7253d9fad82d9a
Wellington Barbosa <welitonpe@gmail.com>
2022-01-24 14:12:56 -0300

    wip

--- index.js
+++ index.js
@@ -1,32 +1,55 @@
 const express = require("express");
-const cors = require("cors");
+const mysql = require("mysql");
+const bodyParser = require("body-parser");
+
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "app",
+});
+
 const path = require("path");
 const app = express();
 const PORT = 5000;
 
-// app.use(cors());
-app.listen(PORT, () => {
-  console.log(`Serve is Running on Port: ${PORT}`);
-});
-
-app.use("/meusite", express.static(path.join(__dirname, "client")));
+app.use("/", bodyParser.json());
+app.use("/app", express.static(path.join(__dirname, "client")));
 
 app.get("/", (req, res) => {
-  console.log(req.body);
-  res.send("<h1>Hello World from GET</h1>");
+  res.type("json");
+
+  con.connect((err) => {
+    if (err) throw err;
+    con.query("SELECT * FROM user", (err, result, fields) => {
+      if (err) throw err;
+      console.log(result);
+      res.send(result);
+    });
+  });
 });
 
 app.post("/", (req, res) => {
-  console.log(req.body);
-  res.send("<h1>Hello World from POST</h1>");
+  con.connect((err) => {
+    if (err) throw err;
+    console.log("Connected!");
+    var sql = `INSERT INTO user (user_name,pass_password,user_email) VALUES('${req.body.name}', '${req.body.senha}', '${req.body.email}')`;
+    con.query(sql, (err, result) => {
+      if (err) throw err;
+      console.log("1 usuario foi adicionado com sucesso");
+    });
+  });
+  res.send(req.body);
 });
 
 app.put("/", (req, res) => {
-  console.log(req.body);
   res.send("<h1>Hello World from PUT</h1>");
 });
 
 app.delete("/", (req, res) => {
-  console.log(req.body);
   res.send("<h1>Hello World from DELETE</h1>");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is Running on port: ${PORT}`);
 });
